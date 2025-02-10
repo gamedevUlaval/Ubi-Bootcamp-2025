42722d1dc5cdb2ecef507ae908c4ee05127c2710
Vincent Vilain <vincent.vilain.1@ulaval.ca>
2025-02-09 23:51:43 -0500

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,9 +1,12 @@
 class PlayerManager
 {
     public string playername;
+    int MaVariableMalNommée = 10;
 
     public void setname(string name)
     {
-        playername = name;
+        
+        playername =name;
+        MaVariableMalNommée  = 20;
     }
 }
