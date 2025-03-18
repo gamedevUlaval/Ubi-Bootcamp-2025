33e1d5a92f04b134c97d93460814207df026fdbb
Vincent Vilain <vincent.vilain.1@ulaval.ca>
2025-03-18 13:27:32 -0400

    feat : add player input handler, player movement and player animation controller

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,46 @@
+using PlayerMovement;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    PlayerMovementController _playerMovementController;
+    Animator _animator;
+    
+    static readonly int VelocityZ = Animator.StringToHash("VelocityZ");
+    static readonly int VelocityX = Animator.StringToHash("VelocityX");
+    
+    bool _isRunning = false;
+    bool _isMoving = true;
+    
+    void Awake()
+    {
+        _playerMovementController = GetComponent<PlayerMovementController>();
+        _animator = GetComponent<Animator>();
+    }
+    
+    void FixedUpdate()
+    {
+        UpdateAnimator();
+    }
+    
+    void UpdateAnimator()
+    {
+        if (!_isMoving) return;
+        
+        Vector3 moveDirection = _playerMovementController.GetPlayerMovement();
+        
+        if (moveDirection.magnitude > 0.01f)
+        {
+            float velocityX = Vector3.Dot(moveDirection.normalized, transform.forward);
+            float velocityZ = Vector3.Dot(moveDirection.normalized, transform.right);
+        
+            _animator.SetFloat(VelocityZ, velocityZ, 0.1f, Time.deltaTime);
+            _animator.SetFloat(VelocityX, velocityX, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            _animator.SetFloat(VelocityZ, 0f, 0.1f, Time.deltaTime);
+            _animator.SetFloat(VelocityX, 0f, 0.1f, Time.deltaTime);
+        }
+    }
+}
