7d1be5176e2b916a2c8bcdbfe6bb7af3d457f7b6
Sophie Nayebi <sophie.nayebi.1@ulaval.ca>
2025-03-23 23:50:26 -0400

    Test merge

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,14 @@ public class Player : NetworkTransform
     public float Speed = 10;
     public bool ApplyVerticalInputToZAxis;
     private Vector3 m_Motion;
+
+    enum PlayerType
+    {
+        HUMAN, GHOST
+    }
     
+    private PlayerType playerType;
+
     void Start()
     {
         if (!HasAuthority)
@@ -17,47 +24,17 @@ public class Player : NetworkTransform
             return;
         }
         
-        GameObject prefab;
         if (!CurrentPlayer.ReadOnlyTags().Contains("INIT_GHOST") && (NetworkManager.ConnectedClients.Count == 1 || CurrentPlayer.ReadOnlyTags().Contains("INIT_HUMAN")))
         {
-            prefab = Resources.Load<GameObject>("Human");
+            playerType = PlayerType.HUMAN;
+            tag = "Human";
         }
         else
         {
-            prefab = Resources.Load<GameObject>("Ghost");
+            playerType = PlayerType.GHOST;
+            tag = "Ghost";
         }
-        var instance = Instantiate(prefab, transform);
-        var instanceNetworkObject = instance.GetComponent<NetworkObject>();
-        instanceNetworkObject.Spawn();
+        
     }
-
-    private void Update()
-    {
-        // If not spawned or we don't have authority, then don't update
-        if (!IsSpawned || !HasAuthority)
-        {
-            return;
-        }
-
-        // Handle acquiring and applying player input
-        m_Motion = Vector3.zero;
-        m_Motion.x = Input.GetAxis("Horizontal");
-
-        // Determine whether the vertical input is applied to the Y or Z axis
-        if (!ApplyVerticalInputToZAxis)
-        {
-            m_Motion.y = Input.GetAxis("Vertical");
-        }
-        else
-        {
-            m_Motion.z = Input.GetAxis("Vertical");
-        }
-
-        // If there is any player input magnitude, then apply that amount of
-        // motion to the transform
-        if (m_Motion.magnitude > 0)
-        {
-            transform.position += m_Motion * Speed * Time.deltaTime;
-        }
-    }
+    
 }
